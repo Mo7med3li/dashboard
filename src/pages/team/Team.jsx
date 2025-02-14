a8d30566f1df9ae5897ff9f84bc737ae8c977f32
nohamed3li <mmkandeelz74@gmail.com>
2025-02-14 06:42:27 +0200

    design team page

--- src/pages/team/Team.jsx
+++ src/pages/team/Team.jsx
@@ -1,5 +1,110 @@
-import React from "react";
-
+import { DataGrid } from "@mui/x-data-grid";
+import { rows } from "./data";
+import { Box, Typography, useTheme } from "@mui/material";
+import AdminPanelSettingsOutlinedIcon from "@mui/icons-material/AdminPanelSettingsOutlined";
+import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined";
+import SecurityOutlinedIcon from "@mui/icons-material/SecurityOutlined";
+import { red } from "@mui/material/colors";
 export default function Team() {
-  return <div>Team page</div>;
+  const theme = useTheme();
+  const columns = [
+    {
+      field: "ID",
+      headerName: "ID",
+
+      align: "center",
+      headerAlign: "center",
+      width: 33,
+    },
+    {
+      field: "Name",
+      headerName: "Name",
+      flex: 1,
+      align: "center",
+      headerAlign: "center",
+    },
+    {
+      field: "Email",
+      headerName: "Email",
+      flex: 1,
+      align: "center",
+      headerAlign: "center",
+    },
+    {
+      field: "Age",
+      headerName: "Age",
+
+      align: "center",
+      headerAlign: "center",
+    },
+    {
+      field: "Phone",
+      headerName: "Phone",
+      flex: 1,
+      align: "center",
+      headerAlign: "center",
+    },
+    {
+      field: "Access",
+      headerName: "Access",
+      flex: 1,
+      align: "center",
+      headerAlign: "center",
+      renderCell: ({ row: { Access } }) => {
+        return (
+          <>
+            <Box
+              sx={{
+                p: "8px",
+                mx: "auto",
+                width: "99px",
+                bgcolor:
+                  Access === "User"
+                    ? theme.palette.primary.dark
+                    : Access === "Admin"
+                    ? theme.palette.secondary.dark
+                    : theme.palette.success.dark,
+                borderRadius: "5px",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-evenly",
+              }}
+            >
+              {Access === "User" ? (
+                <LockOpenOutlinedIcon
+                  fontSize="small"
+                  sx={{ color: theme.palette.grey[100] }}
+                />
+              ) : Access === "Admin" ? (
+                <AdminPanelSettingsOutlinedIcon
+                  fontSize="small"
+                  sx={{ color: theme.palette.grey[100] }}
+                />
+              ) : (
+                <SecurityOutlinedIcon
+                  fontSize="small"
+                  sx={{ color: theme.palette.grey[100] }}
+                />
+              )}
+              <Typography
+                sx={{ fontSize: "15px" }}
+                color={theme.palette.grey[100]}
+              >
+                {Access}
+              </Typography>
+            </Box>
+          </>
+        );
+      },
+    },
+  ];
+  return (
+    <div style={{ height: 600, width: "98%", marginInline: "auto" }}>
+      <DataGrid
+        rows={rows}
+        // @ts-ignore
+        columns={columns}
+      />
+    </div>
+  );
 }
---
+++ src/pages/team/data.js
@@ -0,0 +1,33 @@
+//* id==> unique key, col1==> column1, col2==> column2, col3==> column3
+
+//* col4==> column4, col5==> column5, col6==> column6
+export const rows = [
+  {
+    id: 1,
+    ID: "1",
+    Name: "Mohamed",
+    Email: "Mohamed@gmail.com",
+    Age: "21",
+    Phone: "01003029886",
+    Access: "Admin",
+  },
+  {
+    id: 2,
+    ID: "2",
+    Name: "Ali",
+    Email: "Ali@gmail.com",
+    Age: "21",
+    Phone: "01003029886",
+    Access: "User",
+  },
+  {
+    id: 3,
+    ID: "3",
+    Name: "Mohamed Ali",
+    Email: "Mohamed Ali@gmail.com",
+    Age: "40",
+    Phone: "01003029886",
+    Access: "Manger",
+  },
+];
+// * field==> key, headerName==> column name, width==> column width flex==> column width align==> column text align headerAlign==> column header text align
